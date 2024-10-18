62e43d10be917bb921305b189c471e46607db707
Alan00662 <alan_rd@radiomasterrc.com>
2024-10-18 09:31:17 +0800

    add serial led driver

---
+++ keil_project/BSP/serial_led.c
@@ -0,0 +1,6 @@
+#include "serial_led.h"
+
+void serial_led_init(void)
+{
+    // Initialize the serial LED
+}
---
+++ keil_project/BSP/serial_led.h
@@ -0,0 +1,7 @@
+#ifndef __SERIAL_LED_H__
+#define __SERIAL_LED_H__
+
+#include "main.h"
+
+
+#endif //__SERIAL_LED_H__
